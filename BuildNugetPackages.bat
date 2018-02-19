f8e005745e6afa880bf16a9ea97258c227233c18
Andrew Piper <piper.andrew@spcollege.edu>
2018-02-19 10:05:00 -0500

    Added license and build script.

---
+++ BuildNugetPackages.bat
@@ -0,0 +1,13 @@
+set BASE=%~dp0
+set SEARCH=%BASE%
+set OUT=%BASE%nuget\
+rmdir %OUT%
+mkdir %OUT%
+pushd %BASE%
+rd /s /q src\Threax.ModelGen\bin
+rd /s /q src\Threax.ModelGen\obj
+dotnet build -c Release
+pushd %SEARCH%
+FOR /R %SEARCH% %%I in (Release\*.nupkg) DO move /Y %%I %OUT%
+popd
+popd
